681acfca19539cf3cc9474a9cbf6df7a4515cf15
Jalen <126164913+Jalenzzz@users.noreply.github.com>
2026-03-04 20:57:00 +0300

    Remove deprecated Gradle flags and fix build sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 
@@ -24,9 +23,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
